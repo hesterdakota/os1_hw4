3b8d98a6e9672515d29853af75c7ed6f5b4374bf
Dakota Hester <=>
2021-12-05 13:31:00 -0600

    removed debugging messages

--- lists.h
+++ lists.h
@@ -87,75 +87,53 @@ typedef struct __h_list_T   {
 
 // init function for hand over hand locking list
 void hListInit(h_list_T* h_list)  {
-    printf("initializing list\n");
     h_list->head = NULL; // lock will be iniitilaized upon 1st insert call
-    printf("attemptng to init lock\n");
     pthread_mutex_init(&h_list->head_lock, NULL);
-    printf("lock init\n");
 }
 
 // insert function for hand over hand locking list
 int hListInsert(h_list_T* h_list, int key)   {
-    // printf("entering insert sr\n");
-    // printf("\nEntered insert\n");
     h_node_T* new_h_node = (h_node_T*) malloc(sizeof(h_node_T));
     if (!new_h_node) {
         return -1; // proper error output should be done in main code
     }
 
     // initialize lock
-    // printf("attempting to initialize lock\n");
     pthread_mutex_init(&new_h_node->lock, NULL);
-    // printf("lock created\n");
 
     new_h_node->key = key;
 
-    // critical operations
     pthread_mutex_lock(&h_list->head_lock); // lock head
-    // printf("head lock locked\n");
     new_h_node->next = h_list->head;
     pthread_mutex_unlock(&h_list->head_lock); // unlock
-    // printf("head lock unlocked\n");
 
     h_list->head = new_h_node; // update head of list
-    // printf("head updated\n");
 
     return 0; // success
 }
 
 // lookup function for basic locking list
 int hListLookup(h_list_T* h_list, int key)   {
-    // printf("Entering lookup sr\n");
     int return_val = -1;
 
     if (!h_list->head) return return_val; // Empty list
-
-    // printf("list not empty\n");
+;
     h_node_T* curr = h_list->head;
     h_node_T* next;
 
-    // printf("locking current node\n");
     pthread_mutex_lock(&curr->lock); // lock head node
-   //  printf("locked current node\n");
 
     while (curr)    {
-        // printf("loop entered\n");
         if (curr->key == key)   { // ciritcial operation
-        // printf("match\n");
             return_val = 0;
-            // printf("unlocking current node\n");
             pthread_mutex_unlock(&curr->lock); // unlock
-            // printf("current node unlocked\n");
             break;
         }
         next = curr->next;
-        // printf("updated node\n");
 
         // check if next node exitst
         if (next) {
-            // printf("next node exists, locking it now...\n");
             pthread_mutex_lock(&next->lock);
-            // printf("locked it\n");
         }
     
         pthread_mutex_unlock(&curr->lock);
